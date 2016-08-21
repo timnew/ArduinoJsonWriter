a4dcb10cad0619ef47cc6ee3040d42e398c6f928
Álvaro García <maxpowel@gmail.com>
2016-08-21 23:33:38 +0200

    Added some syntactic sugar to avoid write separators all time

--- JsonWriter.cpp
+++ JsonWriter.cpp
@@ -4,8 +4,104 @@ JsonWriter::JsonWriter(Stream* stream)
 	:stream(stream){
 }
 
+// This function checks if a separator should be printed
+void JsonWriter::ifSeparator(){
+        if(firstElement){
+          firstElement = false;
+        } else {
+          separator();
+        }
+}
+
 JsonWriter& JsonWriter::beginObject(){
+        ifSeparator();
 	stream->print("{");
+        firstElement = true;
+	return *this;
+}
+
+JsonWriter& JsonWriter::beginObject(String name){
+        ifSeparator();
+        memberName(name);
+	stream->print("{");
+        firstElement = true;
+	return *this;
+}
+
+JsonWriter& JsonWriter::property(String name, char* value){
+        ifSeparator();
+        memberName(name);
+        string(value);
+	return *this;
+}
+
+JsonWriter& JsonWriter::property(String name, String value){
+        ifSeparator();
+        memberName(name);
+        string(value);
+	return *this;
+}
+
+JsonWriter& JsonWriter::property(String name, int value){
+        ifSeparator();
+        memberName(name);
+        number(value);
+	return *this;
+}
+
+JsonWriter& JsonWriter::property(String name, unsigned int value){
+        ifSeparator();
+        memberName(name);
+        number(value);
+	return *this;
+}
+
+JsonWriter& JsonWriter::property(String name, long value){
+        ifSeparator();
+        memberName(name);
+        number(value);
+	return *this;
+}
+
+JsonWriter& JsonWriter::property(String name, unsigned long value){
+        ifSeparator();
+        memberName(name);
+        number(value);
+	return *this;
+}
+
+JsonWriter& JsonWriter::property(String name, short value){
+        ifSeparator();
+        memberName(name);
+        number(value);
+	return *this;
+}
+
+JsonWriter& JsonWriter::property(String name, unsigned short value){
+        ifSeparator();
+        memberName(name);
+        number(value);
+	return *this;
+}
+
+JsonWriter& JsonWriter::property(String name, byte value){
+        ifSeparator();
+        memberName(name);
+        number(value);
+	return *this;
+}
+
+JsonWriter& JsonWriter::property(String name, float value){
+        ifSeparator();
+        memberName(name);
+        number(value);
+	return *this;
+}
+
+JsonWriter& JsonWriter::property(String name, double value){
+        ifSeparator();
+        memberName(name);
+        number(value);
 	return *this;
 }
 
@@ -31,21 +127,36 @@ JsonWriter& JsonWriter::endObject(){
 }
 
 JsonWriter& JsonWriter::beginArray(){
+        ifSeparator();
+        firstElement = true;
 	stream->print("[");
 	return *this;
 }
+
+JsonWriter& JsonWriter::beginArray(String name){
+
+        ifSeparator();
+        firstElement = true;
+        memberName(name);
+	stream->print("[");
+	return *this;
+}
+
+
 JsonWriter& JsonWriter::endArray(){
 	stream->print("]");
 	return *this;
 }
 
 JsonWriter& JsonWriter::string(char* text){
+        ifSeparator();
 	stream->print("\"");
 	stream->print(text);
 	stream->print("\"");
 	return *this;
 }
 JsonWriter& JsonWriter::string(String text){
+        ifSeparator();
 	stream->print("\"");
 	stream->print(text);
 	stream->print("\"");
@@ -53,48 +164,60 @@ JsonWriter& JsonWriter::string(String text){
 }
 	
 JsonWriter& JsonWriter::number(int number){
+        ifSeparator();
 	stream->print(number, DEC);
 	return *this;
 }
 JsonWriter& JsonWriter::number(unsigned int number){
+        ifSeparator();
 	stream->print(number, DEC);
 	return *this;
 }
 JsonWriter& JsonWriter::number(long number){
+        ifSeparator();
 	stream->print(number, DEC);
 	return *this;
 }
 JsonWriter& JsonWriter::number(unsigned long number){
+        ifSeparator();
 	stream->print(number, DEC);
 	return *this;
 }
 JsonWriter& JsonWriter::number(short number){
+        ifSeparator();
 	stream->print(number, DEC);
 	return *this;
 }
 JsonWriter& JsonWriter::number(unsigned short number){
+        ifSeparator();
 	stream->print(number, DEC);
 	return *this;
 }
 JsonWriter& JsonWriter::number(byte number){
+        ifSeparator();
 	stream->print(number, DEC);
 	return *this;
 }
-// JsonWriter& JsonWriter::number(float number){
-// 	stream->print(number);
-//  return *this;
-// }
-// JsonWriter& JsonWriter::number(double number){
-// 	stream->print(number);
-//  return *this;
-// }
+
+JsonWriter& JsonWriter::number(float number){
+        ifSeparator();
+ stream->print(number);
+ return *this;
+}
+JsonWriter& JsonWriter::number(double number){
+        ifSeparator();
+ stream->print(number);
+ return *this;
+}
 
 JsonWriter& JsonWriter::null(){
+        ifSeparator();
 	stream->print("null");
 	return *this;
 }
 
 JsonWriter& JsonWriter::boolean(bool value){
+        ifSeparator();
 	stream->print(value ? "true" : "false");
 	return *this;
 }
--- JsonWriter.h
+++ JsonWriter.h
@@ -6,18 +6,37 @@
 class JsonWriter {
 private:
 	Stream* stream;
+        bool firstElement = true;
+        void ifSeparator();
 
 public:
 	JsonWriter(Stream* stream);
 
 	JsonWriter& beginObject();	
+        JsonWriter& beginObject(String name);
 	JsonWriter& memberName(char* name);	
 	JsonWriter& memberName(String name);	
 	JsonWriter& separator();
 	JsonWriter& endObject();
 
 	JsonWriter& beginArray();
+        JsonWriter& beginArray(String name);
 	JsonWriter& endArray();
+
+	JsonWriter& property(String name, char* value);
+        JsonWriter& property(String name, String value);
+        
+	JsonWriter& property(String name, int value);
+	JsonWriter& property(String name, unsigned int value);
+	JsonWriter& property(String name, long int value);
+	JsonWriter& property(String name, unsigned long int value);
+	JsonWriter& property(String name, short value);
+	JsonWriter& property(String name, unsigned short value);
+	JsonWriter& property(String name, byte value);
+        JsonWriter& property(String name, float value);
+        JsonWriter& property(String name, double value);
+
+
 
 	JsonWriter& string(char* text);
 	JsonWriter& string(String text);
@@ -29,12 +48,14 @@ public:
 	JsonWriter& number(short number);
 	JsonWriter& number(unsigned short number);
 	JsonWriter& number(byte number);
-	// JsonWriter& number(float number);
-	// JsonWriter& number(double number);
+	JsonWriter& number(float number);
+	JsonWriter& number(double number);
 
 	JsonWriter& null();
 
 	JsonWriter& boolean(bool value);
+
+        int operator[](char *);
 };
 
 #endif __JSON_WRITER_H__
