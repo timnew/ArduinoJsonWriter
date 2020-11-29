3647b69f91260f6ddecd173e8e4719f67a7118d8
Robert van den Breemen <robert@vandenbreemen.net>
2020-11-29 02:19:33 +0100

    updated examples to be similar

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
-ArduinoJsonWriter
+PrettyJsonWriter
 =================
+An improved fork of ArduinoJsonWriter by Robert van den Breemen (29 november 2020)
 
-November 2020: RvdB: This is fork from the orginal... adding some stuff I need. 
-- fixing the class so the object can be used in loops (begin/end document)
-- adding pretty print 
+Json Writer is a light weight library that make it easier to generate json document on Arduino. 
 
-Json Writer is a light weight library that make it easier to generate json document on Arduino.
+The 2020 edition improved on the orginal by fixing and improving in the following ways: 
+- fixing the class so the object can be used in loops (beginDocument/endDocument)
+- adding pretty print function (even more readable Json for Humans) 
 
-The library works pretty similar to the SAX for XML, it treats JSON as a stream of nodes. With the API, you can compose a JSON document by emitting each node.
-
+The library works pretty similar to the SAX for XML, it treats JSON as a stream of nodes. With the API, you can compose a JSON document by emitting each node to the stream directly not claiming more memory than absolutly needed to generate the JSON. 
 
 Initialize JsonWriter
 ---------------------
--- examples/Nested/Nested.ino
+++ examples/Nested/Nested.ino
@@ -5,38 +5,43 @@ JsonWriter serialJsonWriter(&Serial);
 void setup() {
 
   Serial.begin(115200);
-  serialJsonWriter
-
-  .beginDocument(true, 2)
-    .beginObject()
-    .property("id", 7)
-    .beginObject("name")
-            .property("firstName","James")
-            .property("lastName","Bond")
-        .endObject()
-        .beginArray("languages")
-            .string("English")
-            .string("Russian")
-        .endArray()
-        .beginArray("gadgets")
-            .beginObject()
-                .property("name", "Laser watch")
-                .property("returned", false)
-            .endObject()
-            .beginObject()
-                .property("name", "Walther PP")
-                .property("returned", false)
-                .beginArray("calibers")
-                    .number(32)
-                    .number(380)
-                    .number(22)
-                .endArray()
-            .endObject()
-        .endArray()
-        .endObject()
-    .endDocument();  
-
+  
 }
 
+int i = 0;
 void loop() {
+    delay(1000);
+    i++;
+    Serial.println(i);
+
+    serialJsonWriter
+
+    .beginDocument(true, 2)
+        .beginObject()
+        .property("id", 7)
+        .beginObject("name")
+                .property("firstName","James")
+                .property("lastName","Bond")
+            .endObject()
+            .beginArray("languages")
+                .string("English")
+                .string("Russian")
+            .endArray()
+            .beginArray("gadgets")
+                .beginObject()
+                    .property("name", "Laser watch")
+                    .property("returned", false)
+                .endObject()
+                .beginObject()
+                    .property("name", "Walther PP")
+                    .property("returned", false)
+                    .beginArray("calibers")
+                        .number(32)
+                        .number(380)
+                        .number(22)
+                    .endArray()
+                .endObject()
+            .endArray()
+            .endObject()
+        .endDocument();  
 }
